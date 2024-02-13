6b84d0a6effc4e27ae3ae17a14beb2e8d3919771
Mateleo <mateleo78@gmail.com>
2024-02-13 19:59:34 +0100

    Input

--- pages/index.vue
+++ pages/index.vue
@@ -1,4 +1,8 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+const items = [{
+    
+}];
+</script>
 <template>
   <div>
     <main class="bg-white/20 rounded-2xl p-6 mt-12 flex flex-col">
@@ -9,6 +13,13 @@
         <span class="font-semibold underline decoration-green-500">Homemade</span>
         script for Chocolatey. Ninite on steroids
       </p>
+      <div class="flex flex-col gap-2 mt-12">
+        <input
+          type="text"
+          class="rounded-md bg-white/40 placeholder:text-black/70 py-2 px-3 text-sm focus:ring-2 focus:ring-sky-500 outline-none placeholder:italic"
+          placeholder="Search for an app..."
+        />
+      </div>
     </main>
   </div>
 </template>
