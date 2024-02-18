fdf5924e8d5a8095d0d7dddeb73f52bd33195427
Mateleo <mateleo78@gmail.com>
2024-02-18 18:19:27 +0100

    icons fix

--- composables/software.ts
+++ composables/software.ts
@@ -144,6 +144,12 @@ export const items: item[] = [
     category: "compression",
   },
   {
+    name: "WinRar",
+    chocoName: "winrar",
+    iconName: "https://upload.wikimedia.org/wikipedia/fr/a/aa/WinRAR_logo_big.png",
+    category: "compression",
+  },
+  {
     name: "FFMPEG",
     chocoName: "ffmpeg",
     iconName: "logos:ffmpeg-icon",
@@ -152,7 +158,7 @@ export const items: item[] = [
   {
     name: "VLC",
     chocoName: "vlc",
-    iconName: "mdi:vlc",
+    iconName: "https://upload.wikimedia.org/wikipedia/commons/3/38/VLC_icon.png",
     iconColor: "#EF7700",
     category: "media",
   },
@@ -165,27 +171,30 @@ export const items: item[] = [
   {
     name: "PLEX",
     chocoName: "plex",
-    iconName: "mdi:plex",
+    iconName: "https://static-00.iconduck.com/assets.00/plex-icon-2048x2048-kdgfrhh9.png",
     iconColor: "#282A2D",
     category: "media",
   },
   {
     name: "OBS Studio",
     chocoName: "obs-studio",
-    iconName: "simple-icons:obsstudio",
+    iconName:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/1/14/Open_Broadcaster_Software_Logo.png/1200px-Open_Broadcaster_Software_Logo.png",
     iconColor: "#282A2D",
     category: "media",
   },
   {
     name: "ImageGlass",
     chocoName: "imageglass",
-    iconName: "logos:imageglass",
+    iconName:
+      "https://store-images.s-microsoft.com/image/apps.7368.13944022079119990.d11844ce-f663-4b05-a539-38aa44e53088.376c649b-33d9-419b-acdb-02681ffc0108?h=464",
     category: "image",
   },
   {
     name: "Paint.NET",
     chocoName: "paint.net",
-    iconName: "devicon:paint.net",
+    iconName:
+      "https://content.invisioncic.com/r125076/monthly_2020_07/2128238399_paintneticon(6).png.4b20725c1c9d337627a3a03c9ae7adec.png",
     category: "image",
   },
   {
@@ -197,7 +206,7 @@ export const items: item[] = [
   {
     name: "Inkscape",
     chocoName: "inkscape",
-    iconName: "logos:inkscape",
+    iconName: "https://upload.wikimedia.org/wikipedia/commons/a/a2/Inkscape_logo_%282-colour%29.svg",
     category: "image",
   },
   {
@@ -209,7 +218,8 @@ export const items: item[] = [
   {
     name: "PowerToys",
     chocoName: "powertoys",
-    iconName: "logos:powertoys",
+    iconName:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/2/2b/2020_PowerToys_Icon.svg/langfr-1024px-2020_PowerToys_Icon.svg.png",
     category: "tools",
   },
   {
@@ -223,7 +233,20 @@ export const items: item[] = [
   {
     name: "OpenVPN",
     chocoName: "openvpn",
-    iconName: "simple-icons:openvpn",
+    iconName: "https://cdnlogo.com/logos/o/68/openvpn.svg",
+    category: "tools",
+  },
+  {
+    name: "WizTree",
+    chocoName: "wiztree",
+    iconName: "https://antibodysoftware-17031.kxcdn.com/images/wiztree445x.png",
+    category: "tools",
+  },
+  {
+    name: "qBittorent",
+    chocoName: "qbittorrent",
+    iconName:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/6/66/New_qBittorrent_Logo.svg/240px-New_qBittorrent_Logo.svg.png",
     category: "tools",
   },
   {
@@ -249,27 +272,22 @@ export const items: item[] = [
   {
     name: "Steam",
     chocoName: "steam",
-    iconName: "logos:steam",
+    iconName:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/8/83/Steam_icon_logo.svg/512px-Steam_icon_logo.svg.png",
     category: "games",
   },
   {
     name: "Epic Games Launcher",
     chocoName: "epicgameslauncher",
-    iconName: "cib:epic-games",
-    iconColor:"black",
+    iconName: "https://upload.wikimedia.org/wikipedia/commons/3/31/Epic_Games_logo.svg",
+    iconColor: "black",
     category: "games",
   },
   {
     name: "GoG Galaxy",
     chocoName: "goggalaxy",
-    iconName: "simple-icons:gogdotcom",
-    iconColor:"#9650D1",
+    iconName: "https://cdn.icon-icons.com/icons2/3053/PNG/512/gog_galaxy_alt_macos_bigsur_icon_190150.png",
+    iconColor: "#9650D1",
     category: "games",
-  },
-  {
-    name: "qBittorent",
-    chocoName: "qbittorrent",
-    iconName: "simple-icons:qbittorrent",
-    category: "other",
   },
 ];
